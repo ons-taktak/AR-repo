922feaeeb56d1ff4882a03022a77a2b497b35ae4
ons-taktak <ot503@nyu.edu>
2021-11-01 02:15:57 +0400

    scene management lab

---
+++ SceneManagement/Assets/Scripts/XRSceneTransitionController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XRSceneTransitionController : MonoBehaviour
+{
+    public string scene;
+    public void Transition()
+    {
+        XRSceneTransitionManager.Instance.TransitionTo(scene);
+    }
+}
